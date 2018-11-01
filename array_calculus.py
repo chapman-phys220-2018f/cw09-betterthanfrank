700032ce41d5a4b8b9b195825b8ed96d40cbfaca
Gabriella Nutt <nutt@chapman.edu>
2018-11-01 18:17:14 +0000

    Update

--- array_calculus.py
+++ array_calculus.py
@@ -17,9 +17,13 @@ def gradient(x):
     """"""
     dx = x[1]-x[0]
     N = len(x)
-    e = ((f*(x+dx)-f*(x-dx))/(2*dx))
-    x = np.linspace([n])
-    a = np.dia(np.ones_like(x[:-1]),k =1 )#imputs 1D Array and Outputs 2D look up manipulation
+    x = np.linspace(-10,10, num = 100, endpoint = True)
+    a = np.diag(np.ones_like(x[:-1]),k =1 )#imputs 1D Array and Outputs 2D look up manipulation
     b = np.diag(np.ones_like(x[:-1]),k =-1 )
-    D = (1/(2*dx))(a+b)
-    
+    m = (a+b)
+    m[0][2]=2
+    m[0][0:1] = [-2:2]
+    D = (1/(2*dx))(m)
+    return D
+
+fp= D @ f
